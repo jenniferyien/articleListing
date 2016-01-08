1f5653f75f54d7265f8a808bbfca260d659484fa
Jennifer Yien <jenniferllwu@gmail.com>
2016-01-08 16:17:34 -0500

    loading 10articles for initial file

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,9 @@
 var MainDiv = React.createClass({
   getInitialState: function(){
-    return {articleData: []}
+    return {articleData: [], secondArticleSet: [], currentPage: [], pageCount: 0}
   },
   componentDidMount: function(){
+    //getting two calls to load all articles needed
     $.ajax({
       url: './data/articles.json',
       method: 'GET',
@@ -14,17 +15,52 @@ var MainDiv = React.createClass({
       error: function(xhr, status, error){
         console.log('ajax error', error)
       }
-    })
+    }); //ajax
+
+    $.ajax({
+      url: './data/more-articles.json',
+      method: 'GET',
+      datatype: 'JSON',
+      success:function(data, status, xhr){
+        var moreArticles = JSON.parse(data);
+        this.setState({secondArticleSet: moreArticles})
+      }.bind(this),
+      error: function(xhr, status, error){
+        console.log('second ajax', error)
+      }
+    }); //ajax
+
+  },
+  displayLimit: function(next){
+    var page = 0;
+    if(typeof next == 'number'){
+      page = next
+    };
+    var pageAmount = page + 10
+    var article = this.state.articleData
+    var count = this.state.articleData.length
+    var displays = [];
+    for(var i = page; i < pageAmount; i++){
+        displays.push(article[i])
+    }
+    this.setState({currentPage: displays})
+  },
+  more: function(){
+    var next = this.state.pageCount + 10
+    this.setState({pageCount: next})
+    this.displayLimit(next)
   },
   render: function(){
-    console.log(this.state.articleData.length)
-    var article = this.state.articleData.map(function(item){
+    console.log(this.state.currentPage.length)
+    var article = this.state.currentPage.map(function(item){
       return (
         <li>{item.title}</li>
       )
     });
     return (
       <div>
+      <button onClick={this.displayLimit}>click</button>
+      <button onClick={this.more}>Next</button>
       <h2>Hello </h2>
       <div>{article}</div>
       </div>
