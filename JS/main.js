8c49d787f5de14b87209078ba561367f9f07fb06
Jennifer Yien <jenniferllwu@gmail.com>
2016-01-09 12:40:08 -0500

    load articles 10 at a time

--- JS/main.js
+++ JS/main.js
@@ -1,57 +1,58 @@
 var MainDiv = React.createClass({
   getInitialState: function(){
-    return {articleData: [], secondArticleSet: [], currentPage: [], pageCount: 0}
+    //state where data will be stored
+    return {currentPage: [], pageCount: 0}
   },
   componentDidMount: function(){
+    this.ajaxCalls(0)
+  },
+  ajaxCalls: function(num){
+    var pageAmount = num + 10
     //getting two calls to load all articles needed
     $.ajax({
       url: './data/articles.json',
       method: 'GET',
       datatype: 'JSON',
       success: function(data, status, xhr){
+        //needs to parse through json data
         var articles = JSON.parse(data)
-        this.setState({articleData: articles})
+        var displays = [];
+        if(num < articles.length){
+          for(var i = num; i < pageAmount; i++){
+              displays.push(articles[i])
+          }
+        } else {
+          $.ajax({
+            url: './data/more-articles.json',
+            method: 'GET',
+            datatype: 'JSON',
+            async: false,
+            success:function(data, status, xhr){
+              //needs to parse through json data
+              var moreArticles = JSON.parse(data);
+              for(var i = (num-30); i < (pageAmount-30); i++){
+                  displays.push(moreArticles[i])
+              }
+            },
+            error: function(xhr, status, error){
+              console.log('second ajax', error)
+            }
+          }); //ajax
+        }
+        this.setState({currentPage: displays})
       }.bind(this),
       error: function(xhr, status, error){
         console.log('ajax error', error)
       }
     }); //ajax
 
-    $.ajax({
-      url: './data/more-articles.json',
-      method: 'GET',
-      datatype: 'JSON',
-      success:function(data, status, xhr){
-        var moreArticles = JSON.parse(data);
-        this.setState({secondArticleSet: moreArticles})
-      }.bind(this),
-      error: function(xhr, status, error){
-        console.log('second ajax', error)
-      }
-    }); //ajax
-
-  },
-  displayLimit: function(next){
-    var page = 0;
-    if(typeof next == 'number'){
-      page = next
-    };
-    var pageAmount = page + 10
-    var article = this.state.articleData
-    var count = this.state.articleData.length
-    var displays = [];
-    for(var i = page; i < pageAmount; i++){
-        displays.push(article[i])
-    }
-    this.setState({currentPage: displays})
   },
   more: function(){
     var next = this.state.pageCount + 10
     this.setState({pageCount: next})
-    this.displayLimit(next)
+    this.ajaxCalls(next)
   },
   render: function(){
-    console.log(this.state.currentPage.length)
     var article = this.state.currentPage.map(function(item){
       return (
         <li>{item.title}</li>
@@ -59,7 +60,6 @@ var MainDiv = React.createClass({
     });
     return (
       <div>
-      <button onClick={this.displayLimit}>click</button>
       <button onClick={this.more}>Next</button>
       <h2>Hello </h2>
       <div>{article}</div>
